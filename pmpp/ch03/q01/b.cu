ad697a19efe7f0432daf5df2a3ff08469ef96952
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-12-28 05:00:26 +0000

    Add b.cu

---
+++ pmpp/ch03/q01/b.cu
@@ -0,0 +1,68 @@
+
+#include <array>
+#include <iostream>
+
+// For conciseness, assume int is 32-bit
+using u32 = unsigned int;
+
+__global__ void matmul_by_col(const float *pA, const float *pB, float *pC,
+                              const int M, const int K, const int N) {
+  // MxK @ KxN
+  const u32 col = blockIdx.x * blockDim.x + threadIdx.x;
+  if (col < M) {
+    // Col of the B matrix
+    for (u32 row = 0; row < M; ++row) {
+      float acc = 0.0f;
+      for (u32 k = 0; k < K; ++k) {
+        acc += pA[row * K + k] * pB[k * N + col];
+      }
+      pC[row * N + col] = acc;
+    }
+  }
+}
+
+int main() {
+  constexpr int M = 4;
+  constexpr int K = 4;
+  constexpr int N = 4;
+
+  constexpr int blockSize = 256;
+  constexpr int gridSize = (N + blockSize - 1) / blockSize;
+
+  std::array<float, M * K> A{}; // Zero-initialize all elements
+  std::array<float, K * N> B{};
+  std::array<float, M * N> C{};
+  // With A defined this way, A@B = C will be B flipped
+  for (int row = 0; row < std::min(M, K); ++row) {
+    A[row * K + K - 1 - row] = 1.0f;
+  }
+  for (int i = 0; i < B.size(); ++i) {
+    B[i] = i;
+  }
+  // Allocate and copy to device
+  float *pAdev, *pBdev, *pCdev;
+  cudaMalloc(&pAdev, A.size() * sizeof(float));
+  cudaMalloc(&pBdev, B.size() * sizeof(float));
+  cudaMalloc(&pCdev, C.size() * sizeof(float));
+  cudaMemcpy(pAdev, A.data(), A.size() * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(pBdev, B.data(), B.size() * sizeof(float), cudaMemcpyHostToDevice);
+  matmul_by_col<<<gridSize, blockSize>>>(pAdev, pBdev, pCdev, M, K, N);
+  cudaDeviceSynchronize();
+  cudaError_t err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    std::cerr << "CUDA error: " << cudaGetErrorString(err) << std::endl;
+    return 1;
+  }
+  cudaMemcpy(C.data(), pCdev, C.size() * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaFree(pAdev);
+  cudaFree(pBdev);
+  cudaFree(pCdev);
+  // Print result
+  for (int row = 0; row < M; ++row) {
+    for (int col = 0; col < N; ++col) {
+      std::cout << C[row * N + col] << " ";
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
